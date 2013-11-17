0a0739ffcf98019fb27b9fd51c9a56970c72b06b
joneshf <jones3.hardy@gmail.com>
2013-11-17 15:38:01 -0800

    Added not.

--- Hinquire.hs
+++ Hinquire.hs
@@ -7,10 +7,13 @@ This is like a test to see if I can make inquire's ideal typecheck properly.
 -- rel and bool should not be strings, they should have their own types.
 data Relation = Equal | NEqual | GThan | GThanE | LThan | LThanE
 
-data Boolean = And | Or
+data GBool = And | Or
+
+data WBool = NoBool | AndNot | Not
 
 data Inquire = Predicate {key :: String, val :: String, rel :: Relation}
-    | Group {bool :: Boolean, i1 :: Inquire, i2 :: Inquire}
+    | Group {bool :: GBool, inq1 :: Inquire, inq2 :: Inquire}
+    | Wrap {maybeNot :: WBool, inq :: Inquire}
 
 instance Show Relation where
     show Equal  = "="
@@ -20,20 +23,30 @@ instance Show Relation where
     show LThan  = "<"
     show LThanE = "<="
 
-instance Show Boolean where
+instance Show GBool where
     show And = "&"
     show Or  = ";"
 
+instance Show WBool where
+    show NoBool = ""
+    -- I'm not sure if `AndNot` is actually needed,
+    -- but I ran into it with the ls version.
+    show AndNot = "&!"
+    show Not    = "!"
+
 instance Show Inquire where
-    show Predicate {key=k, val=v, rel=r} = k ++ show r ++ v
-    show Group     {bool=b, i1=l, i2=r}  =
+    show Predicate {key=k, val=v, rel=r}    = k ++ show r ++ v
+    show Group     {bool=b, inq1=l, inq2=r} =
         "(" ++ show l ++ ")" ++ show b ++ "(" ++ show r ++ ")"
+    show Wrap      {maybeNot=n, inq=i}      = show n ++ "(" ++ show i ++ ")"
 
 main :: IO ()
 main = do
     let q1 = Predicate {key="color", val="red", rel=Equal}
     let q2 = Predicate {key="shape", val="round", rel=NEqual}
-    let q3 = Group {bool=Or, i1=q1, i2=q2}
+    let q3 = Group {bool=Or, inq1=q1, inq2=q2}
+    let notQ3 = Wrap {maybeNot=Not, inq=q3}
     print q1
     print q2
     print q3
+    print notQ3
