6194e4ea8cab03f7e7b47c2d612a96c09ded74f9
joneshf <jones3.hardy@gmail.com>
2013-11-17 15:26:34 -0800

    Gave relations and booleans their own type.

--- Hinquire.hs
+++ Hinquire.hs
@@ -5,20 +5,35 @@ This is like a test to see if I can make inquire's ideal typecheck properly.
 -}
 
 -- rel and bool should not be strings, they should have their own types.
-data Inquire = Predicate {key :: String, val :: String, rel :: String}
-    | Group {bool :: String, i1 :: Inquire, i2 :: Inquire}
-    deriving (Eq)
+data Relation = Equal | NEqual | GThan | GThanE | LThan | LThanE
+
+data Boolean = And | Or
+
+data Inquire = Predicate {key :: String, val :: String, rel :: Relation}
+    | Group {bool :: Boolean, i1 :: Inquire, i2 :: Inquire}
+
+instance Show Relation where
+    show Equal  = "="
+    show NEqual = "!="
+    show GThan  = ">"
+    show GThanE = ">="
+    show LThan  = "<"
+    show LThanE = "<="
+
+instance Show Boolean where
+    show And = "&"
+    show Or  = ";"
 
 instance Show Inquire where
-    show Predicate {key=k, val=v, rel=r} = k++r++v
+    show Predicate {key=k, val=v, rel=r} = k ++ show r ++ v
     show Group     {bool=b, i1=l, i2=r}  =
-        "(" ++ show l ++ ")" ++ b ++ "(" ++ show r ++ ")"
+        "(" ++ show l ++ ")" ++ show b ++ "(" ++ show r ++ ")"
 
 main :: IO ()
 main = do
-    let q1 = Predicate {key="color", val="red", rel="="}
-    let q2 = Predicate {key="shape", val="round", rel="!="}
-    let q3 = Group {bool=";", i1=q1, i2=q2}
+    let q1 = Predicate {key="color", val="red", rel=Equal}
+    let q2 = Predicate {key="shape", val="round", rel=NEqual}
+    let q3 = Group {bool=Or, i1=q1, i2=q2}
     print q1
     print q2
     print q3
